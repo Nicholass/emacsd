727e70ae42aef1ea90ca8bf8528ea95525eefdaf
Mykola Korobko <korobko.nikolay@gmail.com>
2016-12-09 13:02:36 +0200

    cleaning

--- .emacs.d/elpa/flymake-go-20150714.33/flymake-go-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; flymake-go-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("flymake-go.el") (21969 25355 135997 524000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flymake-go-autoloads.el ends here
